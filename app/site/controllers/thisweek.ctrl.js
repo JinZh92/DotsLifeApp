f8fa7695c0b3c83ce693fa3984b02114826f4666
Ychuan33 <felixyan2011@gmail.com>
2016-08-16 15:38:08 -0400

    thisweek

---
+++ app/site/controllers/thisweek.ctrl.js
@@ -0,0 +1,15 @@
+(function(){
+	'use strict';
+
+	angular
+		.module('lifeCalendarApp')
+		.controller('ThisWeekCtrl',ThisWeekCtrl)
+	function ThisWeekCtrl($state){
+		var overVm = this;
+
+	}	
+
+})();
+
+
+
