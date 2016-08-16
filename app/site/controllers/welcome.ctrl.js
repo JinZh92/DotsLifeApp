9b74a66a57a39cc6c833907989d80a48813dfbac
JinZh92 <jinpeng.zh@live.com>
2016-08-16 14:14:27 -0400

    welcome page and partials

--- app/site/controllers/welcome.ctrl.js
+++ app/site/controllers/welcome.ctrl.js
@@ -7,12 +7,14 @@ angular
 	function WelcomeController($state, $timeout){
 		var ctrl = this;
 		var hideleft = false;
+		var lefttext = '';
 
 		ctrl.actionClick = actionClick;
 		ctrl.resetClick = resetClick;
 		ctrl.register = register;
 		ctrl.login = login;
 		ctrl.about = about;
+		ctrl.goHome = goHome;
 
 		function actionClick(){
 			ctrl.hideleft = true;
@@ -21,19 +23,26 @@ angular
 			ctrl.hideleft = false;
 		}
 		function register(){
-			ctrl.resetClick();
+			// ctrl.resetClick();
+			ctrl.lefttext = "Sign up";
 			ctrl.actionClick();
 			$state.go('welcome.register');
 		}
 		function login(){
-			ctrl.resetClick();
+			// ctrl.resetClick();
+			ctrl.lefttext = "Login";
 			ctrl.actionClick();
 			$state.go('welcome.login');
 		}
 		function about(){
-			ctrl.resetClick();
+			// ctrl.resetClick();
+			ctrl.lefttext = "About";
 			ctrl.actionClick();
 			$state.go('welcome.about');
+		}
+		function goHome(){
+			ctrl.resetClick();
+			$state.go('welcome');
 		}
 
 	}
